d7af1e8053c9c4bdb6d46c4eb604b6461048e09e
Shintaro Tamefusa <tameshin@gmail.com>
2020-04-01 23:42:38 +0900

    initial commit

---
+++ src/main/java/com/example/webdemo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.webdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@GetMapping("/")
+	String home() {
+		return "Hello!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
